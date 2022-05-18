cc51c2fb42853c6db2a50a2bb49aded1cbd2dd8b
mrmenorca3 <91108850+mrmenorca3@users.noreply.github.com>
2022-05-18 19:05:03 +0800

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -21,10 +21,13 @@ def render(Week = 0):
             ],
             }    
 
-    fig = px.choropleth(data, geojson=counties, featureidkey="properties.ADM2_EN",
+    fig = px.choropleth_mapbox(data, geojson=counties, featureidkey="properties.ADM2_EN",
                             locations='ADM2_EN', color='cases',
                                color_continuous_scale="Reds",
+                               mapbox_style="carto-positron",
                                labels={'cases':'Number of Cases'},
+                               zoom = 7,
+                               center = {"lat": 14.249701614019127, "lon":  121.18649902198216},
                               )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, title = "Plot Title")
     fig.update_geos(fitbounds="locations")
